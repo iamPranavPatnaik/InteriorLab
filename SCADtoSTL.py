cdc9b7783bbccb13a5bcdc6b8369daab952849f2
Pranav Patnaik <137661401+iamPranavPatnaik@users.noreply.github.com>
2024-07-08 23:32:05 -0400

    Added gitignore, main, conversion SCAD to STL

---
+++ SCADtoSTL.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def convert_scad_to_stl(scad_file, stl_file):
+    # Command to run OpenSCAD with input and output files
+    command = ['openscad', '-o', stl_file, scad_file]
+    try:
+        # Run the command
+        subprocess.run(command, check=True)
+        print(f"Successfully converted {scad_file} to {stl_file}")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+
+scad_file = ''
+stl_file = ''
+convert_scad_to_stl(scad_file, stl_file)
