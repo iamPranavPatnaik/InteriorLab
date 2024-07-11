ba17b10166169b027493e1c72a92facaa5052702
Pranav Patnaik <137661401+iamPranavPatnaik@users.noreply.github.com>
2024-07-10 21:43:13 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,81 +1,86 @@
 from tkinter import *
 
-# root = Tk()
-# root.title('InteriorLAB')
-# root.iconbitmap("interiorLABicon.ico")
-
-BG = "#AAAAAA"
-FG = "#C4C4C4"
-TEXT = "#EDE8E7"
+BG_GRAY = "#ABB2B9"
+BG_COLOR = "#17202A"
+TEXT_COLOR = "#EAECEE"
 
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
 
-
 class GUI:
+    
     def __init__(self):
         self.window = Tk()
         self._setup_main_window()
-    
+        
     def run(self):
         self.window.mainloop()
         
-
     def _setup_main_window(self):
         self.window.title("InteriorLAB")
         self.window.iconbitmap("interiorLABicon.ico")
         self.window.resizable(width=False, height=False)
-        self.window.configure(width=500, height=550, bg=BG)
-
-        # title
-        head_label = Label(self.window, bg=BG, fg=TEXT, text="Welcome", font=FONT_BOLD, pady=10)
+        self.window.configure(width=470, height=550, bg=BG_COLOR)
+        
+        # head label
+        head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
+                           text="Welcome", font=FONT_BOLD, pady=10)
         head_label.place(relwidth=1)
-
-        # divider
-        line = Label(self.window, width=450, bg=BG)
-        line.place(relwidth=1, rely=0.07, relheight=0.01)
-
-        # text
-        self.text_widget = Text(self.window, width=20, height=2, bg=BG, fg=TEXT, font=FONT, padx=5, pady=5)
-        self.text_widget.place(relheight=0.75, relwidth=1, rely=0.08)
-        self.text_widget.configure(cursor='arrow', state=DISABLED)
-
-        # scroll 
+        
+        # tiny divider
+        line = Label(self.window, width=450, bg=BG_GRAY)
+        line.place(relwidth=1, rely=0.07, relheight=0.012)
+        
+        # text widget
+        self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
+                                font=FONT, padx=5, pady=5)
+        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
+        self.text_widget.configure(cursor="arrow", state=DISABLED)
+        
+        # scroll bar
         scrollbar = Scrollbar(self.text_widget)
-        scrollbar.place(relheight=1, relx=0.97)
+        scrollbar.place(relheight=1, relx=0.974)
         scrollbar.configure(command=self.text_widget.yview)
-
+        
         # bottom label
-        bottom_label = Label(self.window, bg=BG, height=80)
-        bottom_label.place(relwidth=1, rely=0.8)
-
-        # msg entry box
-        self.msg_entry = Entry(bottom_label, bg="#000000", fg=TEXT, font=FONT)
-        self.msg_entry.place(relwidth=0.75, relheight=0.06, rely=0.008, relx=0.011)
+        bottom_label = Label(self.window, bg=BG_GRAY, height=80)
+        bottom_label.place(relwidth=1, rely=0.825)
+        
+        # message entry box
+        self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
+        self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
         self.msg_entry.focus()
         self.msg_entry.bind("<Return>", self._on_enter_pressed)
-
+        
         # send button
-        send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG, command=lambda: self._on_enter_pressed(None))
+        send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY,
+                             command=lambda: self._on_enter_pressed(None))
         send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
-
+     
     def _on_enter_pressed(self, event):
         msg = self.msg_entry.get()
         self._insert_message(msg, "You")
-
+        
     def _insert_message(self, msg, sender):
         if not msg:
             return
         
         self.msg_entry.delete(0, END)
-        msg1 = f"{sender}: {msg}"
-
+        msg1 = f"{sender}: {msg}\n\n"
         self.text_widget.configure(state=NORMAL)
         self.text_widget.insert(END, msg1)
         self.text_widget.configure(state=DISABLED)
-
-
-
+        
+        msg2 = f"InteriorAssistant: nice\n\n"
+        self.text_widget.configure(state=NORMAL)
+        self.text_widget.insert(END, msg2)
+        self.text_widget.configure(state=DISABLED)
+        
+        self.text_widget.see(END)
+             
+        
 if __name__ == "__main__":
     gui = GUI()
     gui.run()
+
+
